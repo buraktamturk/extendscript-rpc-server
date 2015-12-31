1c0fb032c355b58b94864ccc980858cfd37f1175
Burak Tamtürk <buraktamturk@gmail.com>
2015-12-31 16:00:00 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+# ExtendScript RPC Server
+
+```
+npm install extendscript-rpc-server
+```
+
+ExtendScript has Socket class with it's limitations. It's inefficient and *boring* to implement HTTP library on top of it.
+
+Since server-side JavaScript is popular nowadays, I decided to write a *XML / Binary* RPC implementation, that is easy to implement it with the ExtendScript's Socket class.
+
+This library uses XML as a text exchange method. ExtendScript has a nice native (and fast) XML implementation but does not implement native JSON parser. I must agree that there are few over the internet, but some of them are slow and does not implement UTF-8 support. Some of users just use eval function to deserialize it (insecure but genius), however this does not work well on big JSONs.
+
+However, this library can also transport binary alongside with XML, just like multipart datas, but fast.
+
+# Example
+
+**Example Client**
+
+```JavaScript
+var connection = new xmlconnection("localhost:3333");
+
+var response = connection.send([<Login username="burak"
+                                        password="notasecret" />]);
+alert(response[0].toXMLString());
+```
+
+**Example Server**
+
+```JavaScript
+var esrpc = require('../extendscript-rpc-server/index'),
+    et = require('elementtree'),
+    XML = et.XML,
+    ElementTree = et.ElementTree,
+    element = et.Element,
+    subElement = et.SubElement;
+
+var tablo = {
+  "Login": function(data, session, binaries) {
+    // session is connection session, it's shared every every connection
+    // session.authenticed = true;
+
+    // if there are binaries sent from the client, it is stored on binaries array.
+
+    var result = element('UserInfo');
+
+    subElement(result, 'UserName').text = 'Burak Tamtürk';
+
+    return Promise.resolve(result);
+  },
+  "SomeOtherFunction": function(data, session, binaries) {
+    // etc.
+  }
+};
+
+var server = esrpc(tablo);
+
+server.listen({
+  port: 3333
+});
+```
+
+# The MIT License
+
+Copyright (c) 2015 Burak Tamtürk
+
+> Permission is hereby granted, free of charge, to any person obtaining a copy
+> of this software and associated documentation files (the "Software"), to deal
+> in the Software without restriction, including without limitation the rights
+> to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+> copies of the Software, and to permit persons to whom the Software is
+> furnished to do so, subject to the following conditions:
+>
+> The above copyright notice and this permission notice shall be included in
+> all copies or substantial portions of the Software.
+>
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+> IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+> FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+> AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+> LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+> OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+> SOFTWARE.
---
+++ index.js
@@ -0,0 +1,137 @@
+
+var net = require('net'),
+    et = require('elementtree'),
+    ElementTree = et.ElementTree;
+
+module.exports = function(functionTable) {
+  return net.createServer(function (socket) {
+    socket.setEncoding('binary');
+
+    var type = 0,
+        remain = 4,
+        data = "",
+        session = {};
+
+    socket.on('error', function(data) {
+      socket.end();
+    });
+
+    socket.on('data', function(paket) {
+        var offset = 0;
+        while(offset != paket.length) {
+          var offset2 = Math.min(paket.length - offset, remain);
+
+          data += paket.slice(offset, offset + offset2);
+          remain -= offset2;
+
+          offset += offset2;
+
+          if(type == 0 && remain == 0) {
+            type = 1;
+            remain = (data.charCodeAt(0) << 24) | (data.charCodeAt(1) << 16) | (data.charCodeAt(2) << 8) | (data.charCodeAt(3));
+            data = "";
+
+            offset2 = Math.min(paket.length - offset, remain);
+            data += paket.slice(offset, offset + offset2);
+            remain -= offset2;
+            offset += offset2;
+          }
+
+          if(remain == 0) {
+            var offset3 = 0,
+                promises = [],
+                cids = [],
+                datas = [];
+
+            do {
+              var nextSize = (data.charCodeAt(offset3) << 24) | (data.charCodeAt(offset3 + 1) << 16) | (data.charCodeAt(offset3 + 2) << 8) | (data.charCodeAt(offset3 + 3));
+              offset3 += 4;
+
+              var nextContentId = (data.charCodeAt(offset3) << 24) | (data.charCodeAt(offset3 + 1) << 16) | (data.charCodeAt(offset3 + 2) << 8) | (data.charCodeAt(offset3 + 3));
+              offset3 += 4;
+
+              var type2 = data.charCodeAt(offset3);
+              offset3 += 1;
+
+              switch(type2) {
+                case 1:
+                  var xmlDATA = et.parse(new Buffer(data.slice(offset3, nextSize + offset3), 'binary').toString('utf8'));
+
+                  var fnc = functionTable[xmlDATA._root.tag];
+
+                  if(fnc) {
+                    promises.push(fnc(xmlDATA, session, datas));
+                    cids.push(nextContentId);
+                  }
+
+                  break;
+                case 2:
+                  datas.push({
+                    cid: nextContentId,
+                    data: data.slice(offset3, nextSize + offset3)
+                  });
+                  break;
+              }
+
+              offset3 += nextSize;
+            } while(offset3 != data.length);
+
+            Promise.all(promises)
+              .then(function(values) {
+                values = values.map(function(value, index) {
+                  if(!value.tag) {
+                    return { cid: cids[index], type: 2, data: value.data, length: value.data.length };
+                  } else {
+                    var etree = new ElementTree(value);
+                    var xml = etree.write({'xml_declaration': false});
+                    //var len = Buffer.byteLength(xml, 'utf8');
+                    return { cid: cids[index], type: 1, data: xml, length: xml.length };
+                  }
+                });
+
+                var buffer = null;
+                var totalLength = 0;
+
+                for(var i = 0; i < values.length; ++i) {
+                  totalLength += values[i].length + 9;
+                }
+
+                var payload = String.fromCharCode((totalLength >> 24) & 0xFF) +
+                    String.fromCharCode((totalLength >> 16) & 0xFF) +
+                    String.fromCharCode((totalLength >> 8) & 0xFF) +
+                    String.fromCharCode(totalLength & 0xFF);
+
+                socket.write(payload, 'binary');
+
+                values.forEach(function(value) {
+                  var payload2 = String.fromCharCode((value.length >> 24) & 0xFF) +
+                      String.fromCharCode((value.length >> 16) & 0xFF) +
+                      String.fromCharCode((value.length >> 8) & 0xFF) +
+                      String.fromCharCode(value.length & 0xFF) +
+                      String.fromCharCode((value.cid >> 24) & 0xFF) +
+                      String.fromCharCode((value.cid >> 16) & 0xFF) +
+                      String.fromCharCode((value.cid >> 8) & 0xFF) +
+                      String.fromCharCode(value.cid & 0xFF) +
+                      String.fromCharCode(value.type);
+
+                  socket.write(payload2, 'binary');
+                  if(value.type == 1) {
+                    socket.write(value.data, 'utf-8');
+                  } else {
+                    socket.write(value.data, 'binary');
+                  }
+                });
+              });
+
+            type = 1;
+            remain = 4;
+            data = "";
+          }
+        }
+    });
+
+    socket.on('end', function() {
+
+    });
+  });
+};
